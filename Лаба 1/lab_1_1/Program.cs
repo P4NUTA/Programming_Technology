62319a08751a79c897f99d5378ed63d3f8c37771
Павел <PaulEfimov2001@yandex.ru>
2020-02-19 15:18:28 +0300

    first ex

---
+++ Лаба 1/lab_1_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace lab_1_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string myName;
+            Console.WriteLine("Please enter your name");
+            myName = Console.ReadLine();
+            Console.WriteLine("Hello, {0}", myName);
+        }
+    }
+}
