38bd6cc6665cd044de5ccd3427fc4155e273ec0b
Pavel <PaulEfimov2001@yandex.ru>
2020-03-06 15:35:55 +0300

    Lab 1 done without doc

---
+++ Лаба 1/lab_1_4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace lab_1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the first integer");
+                string temp = Console.ReadLine();
+                int i = Int32.Parse(temp);
+                Console.WriteLine("Please enter the second integer");
+                temp = Console.ReadLine();
+                int j = Int32.Parse(temp);
+                int k = i / j;
+                Console.WriteLine("k = {0}", k);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("An format exception was thrown: {0}", e.Message);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("An DivideByZero exception was thrown: {0}", e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An exception was thrown: {0}", e.Message);
+            }
+
+        }
+    }
+}
---
+++ Лаба 1/lab_1_5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lab_1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the triangle perimeter");
+                string temp = Console.ReadLine();
+                double P = Double.Parse(temp);
+                if (P <= 0)
+                {
+                    throw new Exception("Perimeter less than 0");
+                }
+                double a = P / 3;
+                double S = Math.Sqrt(P * Math.Pow((P - a), 3));
+                Console.WriteLine("Side\tSquare");
+                Console.WriteLine("{0:F}\t{1:F}", a, S);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An exception was thrown: {0}", e.Message);
+            }
+        }
+    }
+}
