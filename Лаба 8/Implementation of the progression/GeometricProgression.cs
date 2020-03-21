633e57ed67c9d839553d40a4f01515b9084e441e
Pavel <PaulEfimov2001@yandex.ru>
2020-03-22 01:29:07 +0300

    lab_8_2

---
+++ Лаба 8/Implementation of the progression/GeometricProgression.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Implementation_of_the_progression
+{
+    class GeometricProgression : Progression
+    {
+        // Конструктор класса
+        public GeometricProgression (int K) : base(K) { }
+        // Переписанный метод
+        public override int GetElement()
+        {
+            int sum = 1;
+            for (int i = 1; i <= k; i++)
+                sum *= i;
+            return sum;
+        }
+    }
+}
---
+++ Лаба 8/Implementation of the progression/Progression.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Implementation_of_the_progression
+{
+    abstract class Progression
+    {
+        // Переменная
+        public int k { get; set; }
+        // Конструктор класса
+        public Progression(int K)
+        {
+            this.k = K;
+        }
+        // Абстрактный метод
+        abstract public int GetElement();
+    }
+}
